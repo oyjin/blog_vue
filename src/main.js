5b4d9ea6922fc392d350b421aae19e4ba74096c8
123 <a123@123s-MacBook-Pro.local>
2022-07-02 02:55:23 +0800

    vue前端框架搭建

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+import Vue from "vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+
+Vue.prototype.$axios = axios;
 
 createApp(App).use(store).use(router).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
